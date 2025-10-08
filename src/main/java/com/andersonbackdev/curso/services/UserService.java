7328cf8c4fa9c8d36195f78e6db8ff6f2622aeed
anderson <andersonbc97@gmail.com>
2025-10-08 20:41:24 -0300

    users insert

--- src/main/java/com/andersonbackdev/curso/services/UserService.java
+++ src/main/java/com/andersonbackdev/curso/services/UserService.java
@@ -22,6 +22,9 @@ public class UserService {
     public User findById(Long id) {
         Optional<User> obj = userRepository.findById(id);
         return obj.get();
+    }
 
+    public User insert(User obj){
+        return userRepository.save(obj);
     }
 }
