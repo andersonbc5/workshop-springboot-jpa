389899b611352245d7d614863c1252fe14684b79
anderson <andersonbc97@gmail.com>
2025-10-08 20:15:43 -0300

    payment one-to-one

---
+++ src/main/java/com/andersonbackdev/curso/entities/Payment.java
@@ -0,0 +1,69 @@
+package com.andersonbackdev.curso.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant momement;
+
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Payment() {
+    }
+
+    public Payment(Long id, Instant momement, Order order) {
+        this.id = id;
+        this.momement = momement;
+        this.order = order;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMomement() {
+        return momement;
+    }
+
+    public void setMomement(Instant momement) {
+        this.momement = momement;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Payment payment = (Payment) o;
+        return Objects.equals(id, payment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
