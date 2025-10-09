7f9857bb670ceb2ad9ed81c0abc98182d8a24f50
anderson <andersonbc97@gmail.com>
2025-10-09 20:12:02 -0300

    Exception - findById

--- src/main/java/com/andersonbackdev/curso/services/UserService.java
+++ src/main/java/com/andersonbackdev/curso/services/UserService.java
@@ -3,6 +3,7 @@ package com.andersonbackdev.curso.services;
 
 import com.andersonbackdev.curso.entities.User;
 import com.andersonbackdev.curso.repositories.UserRepository;
+import com.andersonbackdev.curso.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -22,7 +23,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
---
+++ src/main/java/com/andersonbackdev/curso/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.andersonbackdev.curso.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. id " + id);
+    }
+}
