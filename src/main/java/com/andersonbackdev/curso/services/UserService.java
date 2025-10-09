92fd5d660769b461baf06586ebb261034b769048
anderson <andersonbc97@gmail.com>
2025-10-09 20:30:55 -0300

    Exception - update

--- src/main/java/com/andersonbackdev/curso/services/UserService.java
+++ src/main/java/com/andersonbackdev/curso/services/UserService.java
@@ -5,6 +5,7 @@ import com.andersonbackdev.curso.entities.User;
 import com.andersonbackdev.curso.repositories.UserRepository;
 import com.andersonbackdev.curso.services.exceptions.DataBaseException;
 import com.andersonbackdev.curso.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,9 +45,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
